1ac30a599e5c4cf095be47775aa7877405c7d225
Victor <777vvv2015@gmail.com>
2025-08-05 16:29:37 +0300

    Initial commit

---
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDao.java
@@ -0,0 +1,14 @@
+package hiber.dao;
+
+import hiber.model.Car;
+import hiber.model.User;
+
+import java.util.List;
+
+public interface UserDao {
+   void add(User user);
+   List<User> listUsers();
+   void addCar(Car car);
+   List<Car> listCars();
+   User getUserByCar(String model, int series);
+}
---
+++ 2.2.1. spring_hibernate/src/main/java/hiber/service/UserService.java
@@ -0,0 +1,15 @@
+package hiber.service;
+
+import hiber.model.Car;
+import hiber.model.User;
+
+import java.util.List;
+
+public interface UserService {
+    void add(User user);
+    List<User> listUsers();
+    void addCar(Car car);
+    List<Car> listCars();
+    User getUserByCar(String model, int series);
+
+}
